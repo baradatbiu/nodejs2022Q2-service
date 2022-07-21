c4dfedb8f0f10307b8795209d86bfa817b51ce9d
Kharlamov Dmitry <newdimaak@gmail.com>
2022-07-21 18:53:38 +0300

    feat: add typeorm connection to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,34 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        synchronize: configService.get('TYPEORM_SYNCHRONIZE'),
+        logging: configService.get('TYPEORM_LOGGING'),
+        migrationsRun: configService.get('TYPEORM_MIGRATIONS_RUN'),
+        migrationsTableName: configService.get('TYPEORM_MIGRATIONS_TABLE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
+    }),
     UserModule,
     TrackModule,
     ArtistModule,
