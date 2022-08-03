e3c470edfd626dc10a0e19790cd0e2498411a2f2
Kharlamov Dmitry <newdimaak@gmail.com>
2022-08-03 21:23:16 +0300

    feat: add global guard and ecxeptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -8,6 +9,7 @@ import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
 import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { options } from './database/config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -22,6 +24,12 @@ import { options } from './database/config';
     AlbumModule,
     FavouriteModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { LoginDto } from './dto/login.dto';
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { Public } from 'src/decorators/public-api';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   @HttpCode(201)
   async signin(@Body() registerDto: RegisterDto): Promise<string> {
     return await this.authService.signin(registerDto);
   }
 
+  @Public()
   @Post('login')
   @HttpCode(200)
   async login(
@@ -21,6 +24,7 @@ export class AuthController {
     return await this.authService.login(loginDto);
   }
 
+  @Public()
   @Post('refresh')
   @HttpCode(200)
   async refresh(
